f02bbd88a5b4b7109fb77a2ae3b4c4bfb40d111c
jeydi243 <kadiongoe@yahoo.fr>
2020-04-02 22:12:32 +0000

    two added

---
+++ lib/views/two.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Two extends StatefulWidget {
+  Two({Key key}) : super(key: key);
+
+  @override
+  _TwoState createState() => _TwoState();
+}
+
+class _TwoState extends State<Two> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text('Two'),
+    );
+  }
+}
