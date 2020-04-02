9778530687534b85bf8667012631f9f112b44819
jeydi243 <kadiongoe@yahoo.fr>
2020-04-02 23:46:56 +0000

    .gitignore fix

--- lib/views/accueil.dart
+++ lib/views/accueil.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'login.dart';
 
